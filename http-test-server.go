bcb0b8e877f97345323ee503e2fb071b3963c976
Mansur Marvanov <nanorobocop@gmail.com>
2018-10-22 09:49:12 +0900

    Separated handlers per functions

--- http-test-server.go
+++ http-test-server.go
@@ -24,33 +24,49 @@ func getHTTPCode(s string) (codeInt int, codeStr string, err error) {
 	return codeInt, codeStr, nil
 }
 
+func handlerEcho(w http.ResponseWriter, r *http.Request) {
+	urlParts := strings.Split(r.URL.Path, "/")
+	fmt.Fprintf(w, "%s", strings.Join(urlParts[2:], "/"))
+}
+
+func handlerIP(w http.ResponseWriter, r *http.Request) {
+	remoteAddr, _, _ := net.SplitHostPort(r.RemoteAddr)
+	fmt.Fprintf(w, "%s", remoteAddr)
+}
+
+func handlerSleep(w http.ResponseWriter, r *http.Request) {
+	urlParts := strings.Split(r.URL.Path, "/")
+	seconds, err := strconv.Atoi(urlParts[2])
+	if err != nil {
+		http.Error(w, "Cannot sleep specified time", 500)
+		return
+	}
+	time.Sleep(time.Duration(seconds) * time.Second)
+	fmt.Fprintf(w, "Slept for %v second(s)", seconds)
+}
+
+func handlerCode(w http.ResponseWriter, r *http.Request) {
+	urlParts := strings.Split(r.URL.Path, "/")
+	if codeInt, codeStr, err := getHTTPCode(urlParts[1]); err == nil {
+		codeMessage := strconv.Itoa(codeInt) + " " + codeStr
+		http.Error(w, codeMessage, codeInt)
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		urlParts := strings.Split(r.URL.Path, "/")
 		if urlParts[1] == "echo" {
-			fmt.Fprintf(w, "%s", strings.Join(urlParts[2:], "/"))
-			return
+			handlerEcho(w, r)
 		} else if r.URL.Path == "/ip" {
-			remoteAddr, _, _ := net.SplitHostPort(r.RemoteAddr)
-			fmt.Fprintf(w, "%s", remoteAddr)
-			return
-		} else if urlParts[1] == "sleep" && len(urlParts) == 3 {
-			seconds, err := strconv.Atoi(urlParts[2])
-			if err != nil {
-				http.Error(w, "Cannot sleep specified time", 500)
-				return
-			}
-			time.Sleep(time.Duration(seconds) * time.Second)
-			fmt.Fprintf(w, "Slept for %v second(s)", seconds)
-			return
-		} else if codeInt, codeStr, err := getHTTPCode(urlParts[1]); err == nil {
-			codeMessage := strconv.Itoa(codeInt) + " " + codeStr
-			http.Error(w, codeMessage, codeInt)
-			return
+			handlerIP(w, r)
+		} else if len(urlParts) == 3 && urlParts[1] == "sleep" {
+			handlerSleep(w, r)
+		} else if _, err := strconv.Atoi(urlParts[1]); err == nil {
+			handlerCode(w, r)
 		} else {
 			fmt.Fprintf(w, "Hello there %s!", r.URL.Path[1:])
-			return
 		}
 	case "POST":
 		buf := new(bytes.Buffer)
