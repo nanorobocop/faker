7d86dcf9f624a9d0f19b376be4329d195a127650
Marvanov Mansur <nanorobocop@gmail.com>
2021-12-12 23:03:53 +0900

    Add metrics namespace

--- metrics.go
+++ metrics.go
@@ -9,30 +9,34 @@ import (
 
 var (
 	inFlightGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "in_flight_requests",
-		Help: "A gauge of requests currently being served by the wrapped handler.",
+		Namespace: "faker",
+		Name:      "in_flight_requests",
+		Help:      "A gauge of requests currently being served by the wrapped handler.",
 	})
 
 	counter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "api_requests_total",
-			Help: "A counter for requests to the wrapped handler.",
+			Namespace: "faker",
+			Name:      "api_requests_total",
+			Help:      "A counter for requests to the wrapped handler.",
 		}, []string{"code", "handler", "method"},
 	)
 
 	duration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "request_duration_seconds",
-			Help:    "A histogram of latencies for requests.",
-			Buckets: []float64{.25, .5, 1, 2.5, 5, 10},
+			Namespace: "faker",
+			Name:      "request_duration_seconds",
+			Help:      "A histogram of latencies for requests.",
+			Buckets:   []float64{.25, .5, 1, 2.5, 5, 10},
 		}, []string{"code", "handler", "method"},
 	)
 
 	responseSize = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "response_size_bytes",
-			Help:    "A histogram of response sizes for requests.",
-			Buckets: []float64{200, 500, 900, 1500},
+			Namespace: "faker",
+			Name:      "response_size_bytes",
+			Help:      "A histogram of response sizes for requests.",
+			Buckets:   []float64{200, 500, 900, 1500},
 		}, []string{"code", "handler", "method"},
 	)
 )
